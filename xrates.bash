1308625fd9c742d0194d935bff40cf72068384ae
michaeltd <tsouchlarakis@gmail.com>
2021-07-04 10:13:05 +0300

    1625382785

--- xrates.bash
+++ xrates.bash
@@ -6,9 +6,123 @@
 declare -r sdn="$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
 	sbn="$(basename "$(realpath "${BASH_SOURCE[0]}")")"
 
-source "${sdn}/common.src" || exit 1
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS=$'\t\n'
 
-declare -r myusage="Usage: ${sbn} update_xrates | drop_temps"
+_date(){
+    #Date magic - DONT MESS WITH VOODOO!
+    source /etc/os-release
+    if [[ "${NAME}" =~ BSD$ ]]; then
+	for arg in "${@}"; do
+	    case "${arg}" in
+		-d)
+		    if [[ "${2}" =~ - ]]; then
+			local myargs+=("-j" "-f" "%Y-%m-%d")
+		    elif [[ "${2}" =~ / ]]; then
+			local myargs+=("-j" "-f" "%Y/%m/%d")
+		    elif [[ "${2}" =~ [:alpha:] ]]; then
+			local myargs+=("-j" "-f" "%d %b %Y")
+			local temp="${2:0:2} ${2:3:3} ${2:(-4)}"
+			2="${temp}"
+		    elif [[ "${2}" == "${2//[![:digit:]]}" ]]; then
+			case "${#2}" in
+			    6) local myargs+=("-j" "-f" "%y%m%d");;
+			    8) local myargs+=("-j" "-f" "%Y%m%d");;
+			    10) local myargs+=("-j" "-f" "%s");;
+			    *) return 1;;
+			esac
+		    else
+			return 1
+		    fi;;
+		-u) local myargs+=("${arg}" "-j");;
+		*) local myargs+=("${arg}");;
+	    esac
+	done
+    else
+	local myargs=( "${@}" )
+    fi
+    # Clean up temp sources (source /etc/os-release)
+    unset NAME VERSION VERSION_ID ID ANSI_COLOR PRETTY_NAME CPE_NAME HOME_URL BUG_REPORT_URL
+    date "${myargs[@]}"
+}
+
+declare -r database_dir="${sdn}/databases"
+declare -r data_sources_dir="${sdn}/sources"
+declare -r database_fn="${database_dir}/database.db"
+declare -r time_stamp="$(_date -u +%s)"
+declare -r preview_mode=".mode box\n.headers on\n"
+declare -r myusage="
+Name: ${sbn}
+Usage: ${sbn} update | drop_temps | showme [...] | send_sql 'sql statement'
+Description: Manipulate/Query sqlite ${database_fn}
+Examples: ${sbn} update # Populate ${database_fn} with todays xrates
+	  ${sbn} drop_temps # Maintainance - Drop temp tables/Cleanup dead space
+	  ${sbn} showme defunct # Show old currencies and their last valuation
+	  ${sbn} showme dates # Show dates with eur exchange rates stored
+	  ${sbn} showme dates last # Shows the last date imported
+	  ${sbn} showme 20201023 # Will show ALL xrates for given date (if any)
+	  ${sbn} showme 20201023 eur # Will show all xrates for eur in given date
+	  ${sbn} showme eur gbp # Will show last eur to gbp xrate
+	  ${sbn} showme 20201023 eur gbp # Will show eur xrate to gbp for given date
+	  ${sbn} send_sql 'sql statement' # Sends 'sql statement' to parse through ${database_fn}.
+"
+
+log2err(){ echo -ne "${sbn}: ${*}\n" >&2; }
+
+send_sql() {
+    case "${#}" in
+	1) echo -ne "${1}" | "sqlite3" "${database_fn}";;
+	*) log2err "${FUNCNAME[0]}: Usage: ${sbn} ${FUNCNAME[0]} 'query string to parse'"; return 1;;
+    esac
+}
+
+showme() {
+    set +u
+    local dt='' thisusage="${FUNCNAME[0]}: Usage: ${sbn} ${FUNCNAME[0]} defunct|dates [last]|from_ccy to_ccy|[date |[from_ccy|[to_ccy]]]"
+
+    defunct() {
+	echo -ne "${preview_mode}SELECT * FROM DEF_CCY_XRATES ORDER BY CCY ASC;\n" | "sqlite3" "${database_fn}"
+    }
+
+    dates() {
+	if [[ -z "${1}" ]]; then
+	    echo -ne "${preview_mode}SELECT DT FROM CCY_XRATES GROUP BY DT ORDER BY DT ASC;\n" | "sqlite3" "${database_fn}"
+	elif [[ -n "${1}" && "${1}" == "last" ]]; then
+	    echo -ne ".mode list\n.headers off\nSELECT MAX(DT) AS MAX_DATE FROM CCY_XRATES;\n" | "sqlite3" "${database_fn}"
+	else
+	    log2err "${thisusage}"
+	    return 1
+	fi
+    }
+
+    case "${1}" in
+	dates|defunct) "${@}";;
+	*)
+	    if [[ $# -eq 1 || $# -eq 2 || $# -eq 3 ]]; then
+		if _date -d "${1}" +%F &> /dev/null; then
+		    local dt="$(_date -d "${1}" +%F)"
+		elif [[ "${1//[![:alpha:]]}" == "${1}" && "${#1}" -eq "3" && "${2//[![:alpha:]]}" == "${2}" && "${#2}" -eq "3" ]]; then
+		    local dt="$(_date -d "$(${FUNCNAME[0]} dates last)" +%F)"
+		else
+		    log2err "${FUNCNAME[0]}: You need a valid date. eg: yyyymmdd or yyyy-mm-dd!\n${thisusage}"
+		    return 1
+		fi
+		case "${#}" in
+		    1) echo -ne "${preview_mode}SELECT * FROM XRATES WHERE DT = '${dt}';\n" | "sqlite3" "${database_fn}";;
+		    2)
+			if _date -d "${1}" +%F &> /dev/null; then
+			    echo -ne "${preview_mode}SELECT * FROM XRATES WHERE DT = '${dt}' AND FROM_CCY = '${2^^}';\n" | "sqlite3" "${database_fn}"
+			else
+			    echo -ne "${preview_mode}SELECT * FROM XRATES WHERE DT = '${dt}' AND FROM_CCY = '${1^^}' AND TO_CCY = '${2^^}';\n" | "sqlite3" "${database_fn}"
+			fi;;
+		    3) echo -ne "${preview_mode}SELECT * FROM XRATES WHERE DT = '${dt}' AND FROM_CCY = '${2^^}' AND TO_CCY = '${3^^}';\n" | "sqlite3" "${database_fn}";;
+		esac
+	    else
+		log2err "${thisusage}"; return 1
+	    fi
+    esac
+}
 
 drop_temps(){
     IFS=$' \t\n'
@@ -19,7 +133,10 @@ drop_temps(){
 	[[ "$i" =~ ^_ ]] && local temps+=( "$i" )
     done
 
-    [[ "${#temps[*]}" -eq "0" ]] && log2err "${FUNCNAME[0]}: No temp tables found, Nothing left to do!" && return 1
+    if [[ "${#temps[*]}" -eq "0" ]]; then
+	log2err "${FUNCNAME[0]}: No temp tables found, Nothing left to do!"
+	return 1
+    fi
     
     if tty -s; then
 	log2err "${FUNCNAME[0]}: Dropping tables: ${temps[*]}!"
@@ -37,23 +154,7 @@ drop_temps(){
     fi
 }
 
-update_xrates(){
-    # local -r my_datasrc_dir="${data_sources_dir}/${FUNCNAME[0]}/$(date -d "@${time_stamp}" +%Y)/$(date -d "@${time_stamp}" +%m)/$(date -d "@${time_stamp}" +%d)"
-    # local -r sql_file="${my_datasrc_dir}/statements.sql"
-    # local -r csv_temp="_${time_stamp}_csv"
-    # local -r ccy_temp="_${time_stamp}_ccy"
-
-    # local -r zip_src="https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip"
-    # local -r zip_file="${zip_src##*/}"
-    # local -r csv_file="${zip_file//.zip/.csv}"
-
-    # mkdir -p "${my_datasrc_dir}"
-    # curl -sSL "${zip_src}" > "${my_datasrc_dir}/${zip_file}"
-    # unzip -qod "${my_datasrc_dir}" "${my_datasrc_dir}/${zip_file}"
-
-    # local -r csv_data="$(tail -1 "${my_datasrc_dir}/${csv_file}")"
-    # local -r csv_date="${csv_data%%,*}"
-    # local -r nrm_date="$(date -d "${csv_date}" +%F)"
+update(){
 
     local -r zip_src="https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip"
     local -r zip_file="${zip_src##*/}"
@@ -66,8 +167,8 @@ update_xrates(){
 
     local -r csv_data="$(tail -1 "${tmp_dir}/${csv_file}")"
     local -r csv_date="${csv_data%%,*}"
-    local -r nrm_date="$(date -d "${csv_date}" +%F)"
-    local -r nrm_stmp="$(date -d "${csv_date}" +%s)"
+    local -r nrm_date="$(_date -d "${csv_date}" +%F)"
+    local -r nrm_stmp="$(_date -d "${csv_date}" +%s)"
 
     local -r sql_file="${tmp_dir}/statements.sql"
     local -r csv_temp="_${nrm_stmp}_csv"
@@ -113,7 +214,7 @@ INSERT INTO CCY_XRATES SELECT DT,CCY,VAL FROM ${ccy_temp} ORDER BY CCY ASC;\n" >
 
     "sqlite3" "${database_fn}" < "${sql_file}"
 
-    local -r my_datasrc_dir="${data_sources_dir}/${FUNCNAME[0]}/$(date -d "@${nrm_date}" +%Y)/$(date -d "@${nrm_date}" +%m)/$(date -d "@${nrm_date}" +%d)"
+    local -r my_datasrc_dir="${data_sources_dir}/${FUNCNAME[0]}/$(_date -d "${nrm_stmp}" +%Y)/$(_date -d "${nrm_stmp}" +%m)/$(_date -d "${nrm_stmp}" +%d)"
     
     mkdir -p "${my_datasrc_dir}"
     mv -f "${tmp_dir}"/* "${my_datasrc_dir}/"
@@ -122,6 +223,15 @@ INSERT INTO CCY_XRATES SELECT DT,CCY,VAL FROM ${ccy_temp} ORDER BY CCY ASC;\n" >
     log2err "${FUNCNAME[0]}: Imported xrates for: ${nrm_date}!"
 
     drop_temps
+}
+
+main() {
+    if [[ "${#}" == "0" ]]; then
+	log2err "${FUNCNAME[0]}: ${myusage}"
+	return 1
+    else
+	"${@}"
+    fi
 }
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "${@}"
