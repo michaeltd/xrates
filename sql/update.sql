8c8313cba37a31028c2d8df426790b57eba580d9
michaeltd <tsouchlarakis@gmail.com>
2021-08-12 11:36:30 +0300

    1628757390

--- sql/update.sql
+++ sql/update.sql
@@ -1,40 +1,38 @@
-.import --csv ${my_datasrc_dir}/${csv_file} ${csv_temp}
-
-CREATE TABLE IF NOT EXISTS ${ccy_temp} AS
-       SELECT '${nrm_date}' AS DT, 'EUR' AS CCY, '1.00' AS VAL UNION ALL
-       SELECT '${nrm_date}' AS DT, 'USD' AS CCY, ${csv_temp}.' USD' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'JPY' AS CCY, ${csv_temp}.' JPY' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'BGN' AS CCY, ${csv_temp}.' BGN' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'CZK' AS CCY, ${csv_temp}.' CZK' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'DKK' AS CCY, ${csv_temp}.' DKK' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'GBP' AS CCY, ${csv_temp}.' GBP' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'HUF' AS CCY, ${csv_temp}.' HUF' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'PLN' AS CCY, ${csv_temp}.' PLN' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'RON' AS CCY, ${csv_temp}.' RON' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'SEK' AS CCY, ${csv_temp}.' SEK' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'CHF' AS CCY, ${csv_temp}.' CHF' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'ISK' AS CCY, ${csv_temp}.' ISK' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'NOK' AS CCY, ${csv_temp}.' NOK' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'HRK' AS CCY, ${csv_temp}.' HRK' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'RUB' AS CCY, ${csv_temp}.' RUB' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'TRY' AS CCY, ${csv_temp}.' TRY' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'AUD' AS CCY, ${csv_temp}.' AUD' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'BRL' AS CCY, ${csv_temp}.' BRL' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'CAD' AS CCY, ${csv_temp}.' CAD' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'CNY' AS CCY, ${csv_temp}.' CNY' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'HKD' AS CCY, ${csv_temp}.' HKD' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'IDR' AS CCY, ${csv_temp}.' IDR' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'ILS' AS CCY, ${csv_temp}.' ILS' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'INR' AS CCY, ${csv_temp}.' INR' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'KRW' AS CCY, ${csv_temp}.' KRW' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'MXN' AS CCY, ${csv_temp}.' MXN' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'MYR' AS CCY, ${csv_temp}.' MYR' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'NZD' AS CCY, ${csv_temp}.' NZD' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'PHP' AS CCY, ${csv_temp}.' PHP' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'SGD' AS CCY, ${csv_temp}.' SGD' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'THB' AS CCY, ${csv_temp}.' THB' AS VAL FROM ${csv_temp} UNION ALL
-       SELECT '${nrm_date}' AS DT, 'ZAR' AS CCY, ${csv_temp}.' ZAR' AS VAL FROM ${csv_temp};
-
-DELETE FROM CCY_XRATES WHERE DT = '${nrm_date}';
-
-INSERT INTO CCY_XRATES SELECT DT,CCY,VAL FROM ${ccy_temp} ORDER BY CCY ASC;
+.bail on
+BEGIN TRANSACTION;
+DELETE FROM CCY_XRATES WHERE DT = '2021-08-06';
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'EUR', '1.0000');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'USD', '1.1807');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'JPY', '129.64');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'BGN', '1.9558');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'CZK', '25.413');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'DKK', '7.4371');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'GBP', '0.84840');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'HUF', '352.72');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'PLN', '4.5563');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'RON', '4.9160');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'SEK', '10.1683');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'CHF', '1.0724');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'ISK', '147.80');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'NOK', '10.4230');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'HRK', '7.4978');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'RUB', '86.4194');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'TRY', '10.1385');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'AUD', '1.5968');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'BRL', '6.1594');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'CAD', '1.4763');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'CNY', '7.6379');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'HKD', '9.1827');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'IDR', '16934.12');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'ILS', '3.7967');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'INR', '87.4993');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'KRW', '1349.15');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'MXN', '23.5178');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'MYR', '4.9814');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'NZD', '1.6737');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'PHP', '59.644');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'SGD', '1.5952');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'THB', '39.359');
+INSERT INTO CCY_XRATES VALUES('2021-08-06', 'ZAR', '17.0983');
+COMMIT;
+VACUUM;
--- xrates.bash
+++ xrates.bash
@@ -2,15 +2,31 @@
 #shellcheck source=/dev/null disable=SC2155,SC2034,SC2154
 #
 
-#link free (S)cript: (D)ir(N)ame, (B)ase(N)ame.
-declare -r sdn="$(dirname "$(realpath "${BASH_SOURCE[0]}")")" \
-	sbn="$(basename "$(realpath "${BASH_SOURCE[0]}")")"
-
 # Unofficial Bash Strict Mode
-set -euo pipefail
+set -eo pipefail
 IFS=$'\t\n'
 
-log2err(){ echo -ne "${sbn}: ${*}\n" >&2; }
+#link free (S)cript: (D)ir(N)ame, (B)ase(N)ame.
+declare -r sdn="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+declare -r sbn="$(basename "$(realpath "${BASH_SOURCE[0]}")")"
+declare -r database_dir="${sdn}/databases"
+declare -r database_fn="${database_dir}/database.db"
+declare -r myusage="
+Name: ${sbn}
+Usage: ${sbn} update | send_sql 'sql statement' | showme [...] 
+Description: Manipulate/Query sqlite ${database_fn}
+Examples: ${sbn} update # Populate sqlite with available rates
+	  ${sbn} send_sql 'sql statement' # Sends 'sql statement' to parse through sqlite.
+	  ${sbn} showme defunct # Show old currencies and their last valuation
+	  ${sbn} showme last # Shows latest eur xrates
+	  ${sbn} showme last date # Shows the last date imported
+	  ${sbn} showme dates # Shows all the dates with rates stored
+	  ${sbn} showme eur # Will show all eur xrates for the latest available date
+	  ${sbn} showme eur gbp # Will show last eur to gbp xrate
+	  ${sbn} showme 20201023 # Will show ALL xrates for given date (if any)
+	  ${sbn} showme 20201023 eur # Will show all xrates for eur in given date
+	  ${sbn} showme 20201023 eur gbp # Will show eur xrate to gbp for given date
+"
 
 _date(){
     #Caution: Date magic ahead - DONT MESS WITH VOODOO!
@@ -48,26 +64,7 @@ _date(){
     date "${myargs[@]}"
 }
 
-declare -r database_dir="${sdn}/databases"
-declare -r data_sources_dir="${sdn}/sources"
-declare -r database_fn="${database_dir}/database.db"
-declare -r time_stamp="$(_date -u +%s)"
-declare -r preview_mode=".mode box\n.headers on\n"
-declare -r myusage="
-Name: ${sbn}
-Usage: ${sbn} update | send_sql 'sql statement' | showme [...] 
-Description: Manipulate/Query sqlite ${database_fn}
-Examples: ${sbn} update # Populate sqlite with available rates
-	  ${sbn} send_sql 'sql statement' # Sends 'sql statement' to parse through sqlite.
-	  ${sbn} showme defunct # Show old currencies and their last valuation
-	  ${sbn} showme dates # Show dates with eur exchange rates stored
-	  ${sbn} showme dates last # Shows the last date imported
-	  ${sbn} showme 20201023 # Will show ALL xrates for given date (if any)
-	  ${sbn} showme eur # Will show all eur xrates for the last available date
-	  ${sbn} showme 20201023 eur # Will show all xrates for eur in given date
-	  ${sbn} showme eur gbp # Will show last eur to gbp xrate
-	  ${sbn} showme 20201023 eur gbp # Will show eur xrate to gbp for given date
-"
+log2err(){ echo -ne "${sbn}: ${*}\n" >&2; }
 
 send_sql() {
     case "${#}" in
@@ -77,17 +74,22 @@ send_sql() {
 }
 
 showme() {
-    set +u
-    local dt='' thisusage="${FUNCNAME[0]}: Usage: ${sbn} ${FUNCNAME[0]} defunct|dates [last]|from_ccy to_ccy|[date |[from_ccy|[to_ccy]]]"
+    local dt=''
+    local -r thisusage="${FUNCNAME[0]}: Usage: ${sbn} ${FUNCNAME[0]} defunct|last [date]|dates|from_ccy|from_ccy to_ccy|[date |[from_ccy|[to_ccy]]]"
+    local -r preview_mode=".mode box\n.headers on\n"
 
     defunct() {
 	echo -ne "${preview_mode}SELECT * FROM DEF_CCY_XRATES ORDER BY CCY ASC;\n" | "sqlite3" "${database_fn}"
     }
 
     dates() {
+	echo -ne "${preview_mode}SELECT DT FROM CCY_XRATES GROUP BY DT ORDER BY DT ASC;\n" | "sqlite3" "${database_fn}"
+    }
+
+    last() {
 	if [[ -z "${1}" ]]; then
-	    echo -ne "${preview_mode}SELECT DT FROM CCY_XRATES GROUP BY DT ORDER BY DT ASC;\n" | "sqlite3" "${database_fn}"
-	elif [[ -n "${1}" && "${1}" == "last" ]]; then
+	    echo -ne "${preview_mode}SELECT * FROM LAST_XRATES WHERE FROM_CCY LIKE 'EUR' ORDER BY TO_CCY ASC;\n" | "sqlite3" "${database_fn}"
+	elif [[ -n "${1}" && "${1}" == "date" ]]; then
 	    echo -ne ".mode list\n.headers off\nSELECT MAX(DT) AS MAX_DATE FROM CCY_XRATES;\n" | "sqlite3" "${database_fn}"
 	else
 	    log2err "${thisusage}"
@@ -96,12 +98,10 @@ showme() {
     }
 
     case "${1}" in
-	dates|defunct) "${@}";;
-	-h|--help)
-	    log2err "${thisusage}"
-	    return 1;;
+	-h|--help) log2err "${thisusage}"; return 1;;
+	last|dates|defunct) "${@}";;
 	*)
-	    if [[ $# -eq 1 || $# -eq 2 || $# -eq 3 ]]; then
+	    if [[ "${#}" -eq "1" || "${#}" -eq "2" || "${#}" -eq "3" ]]; then
 		if _date -d "${1}" +%F &> /dev/null; then
 		    local dt="$(_date -d "${1}" +%F)"
 		fi
@@ -131,56 +131,57 @@ showme() {
 }
 
 update() {
-
-    splitstr() {
-	IFS=$'\n' read -d "" -ra arr <<< "${1//${2}/$'\n'}"
-	printf "%s\n" "${arr[*]}"
-    }
-
     local -r zip_src="https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip"
     local -r zip_file="${zip_src##*/}"
     local -r csv_file="${zip_file//.zip/.csv}"
-
+    local -r data_sources_dir="${sdn}/sources"
+    local -r initial_sql="${sdn}/sql/initial.sql"
     local -r tmp_dir="/tmp/${USER}/${$}"
+    local -r zip_fpth="${tmp_dir}/${zip_file}"
+    local -r csv_fpth="${tmp_dir}/${csv_file}"
+    local -r sql_file="${tmp_dir}/${FUNCNAME[0]}.sql"
 
-    mkdir -p "${tmp_dir}"
-    curl -sSL "${zip_src}" > "${tmp_dir}/${zip_file}"
-    unzip -qod "${tmp_dir}" "${tmp_dir}/${zip_file}"
-    
-    local -r csv_flds="$(head -1 "${tmp_dir}/${csv_file}")"
-    local -ar csv_farr=( $(splitstr "${csv_flds}" ',') )
-    local -r csv_data="$(tail -1 "${tmp_dir}/${csv_file}")"
-    local -ar csv_darr=( $(splitstr "${csv_data}" ',') )
+    prep_things() {
+	if [[ "${1}" == "cleanup" ]];then
+	    mkdir -p "${2}"
+	    mv -f "${tmp_dir}"/* "${2}/"
+	    rm -rf "${tmp_dir}/"
+	else
+	    mkdir -p "${tmp_dir}"
+	    curl -sSL "${zip_src}" > "${zip_fpth}"
+	    unzip -qod "${tmp_dir}" "${zip_fpth}"
+	fi
+    }
 
-    local -r csv_date="${csv_data%%,*}"
+    prep_things || { log2err "Deal with errors above and try again!"; return 1; }
+
+    local -r csv_flds="$(head -1 "${csv_fpth}")"
+    local -r csv_data="$(tail -1 "${csv_fpth}")"
+    local -ar csv_farr=( ${csv_flds//,/$'\n'} )
+    local -ar csv_darr=( ${csv_data//,/$'\n'} )
+
+    local -r csv_date="${csv_darr[0]}"
     local -r nrm_date="$(_date -d "${csv_date}" +%F)"
     local -r nrm_stmp="$(_date -d "${csv_date}" +%s)"
+    local -r my_datasrc_dir="${data_sources_dir}/${FUNCNAME[0]}/$(_date -d "${nrm_stmp}" +%Y)/$(_date -d "${nrm_stmp}" +%m)/$(_date -d "${nrm_stmp}" +%d)"
 
     _date -d "${nrm_date}" +%s &> /dev/null || { log2err "No valid date found!"; return 1; }
-    
-    local -r sql_file="${tmp_dir}/${FUNCNAME[0]}.sql"
-    
-    local mysql=".bail on\n"
-    local mysql+="BEGIN TRANSACTION;\n"
+
+    local mysql="$(cat "${initial_sql}")"
+    local mysql+="\n.bail on\nBEGIN TRANSACTION;\n"
     local mysql+="DELETE FROM CCY_XRATES WHERE DT = '${nrm_date}';\n"
     local mysql+="INSERT INTO CCY_XRATES VALUES('${nrm_date}', 'EUR', '1.0000');\n"
     for (( i = 1; i < ${#csv_farr[*]} - 1 ; i++ )); do
 	local mysql+="INSERT INTO CCY_XRATES VALUES('${nrm_date}', '${csv_farr[i]//[[:space:]]}', '${csv_darr[i]//[[:space:]]}');\n"
     done
     local mysql+="COMMIT;\nVACUUM;\n"
-    
-    cat "${sdn}/sql/initial.sql" > "${sql_file}"
 
-    echo -ne "${mysql}" >> "${sql_file}"
+    echo -ne "${mysql}" > "${sql_file}"
 
     "sqlite3" "${database_fn}" < "${sql_file}"
 
-    local -r my_datasrc_dir="${data_sources_dir}/${FUNCNAME[0]}/$(_date -d "${nrm_stmp}" +%Y)/$(_date -d "${nrm_stmp}" +%m)/$(_date -d "${nrm_stmp}" +%d)"
-    
-    mkdir -p "${my_datasrc_dir}"
-    mv -f "${tmp_dir}"/* "${my_datasrc_dir}/"
-    rm -rf "${tmp_dir}/"
-    
+    prep_things "cleanup" "${my_datasrc_dir}"
+
     log2err "${FUNCNAME[0]}: Imported xrates for: ${nrm_date}!"
 }
 
